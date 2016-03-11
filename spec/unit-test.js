c9c1799889d3d9c0dbc7273ffa7eb918753939f3
crystal <793832732@qq.com>
2016-03-11 14:13:56 +0800

    change unit-test var to let

--- spec/unit-test.js
+++ spec/unit-test.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var getNumberArray = require('../main/main.js');
 var getLCDArray = require('../main/main.js');
 var getLCDString = require('../main/main.js');
@@ -5,8 +7,8 @@ var getLCDString = require('../main/main.js');
 describe("when input a number then get array type numberString", function() {
 
   it("when get a number then return a numberArray", function() {
-    var input = 910;
-    var result = ["._.|_|..|", ".....|..|", "._.|.||_|"];
+    let input = 910;
+    let result = ["._.|_|..|", ".....|..|", "._.|.||_|"];
 
     expect(result).toEqual(getNumberArray.getNumberArray(input));
   });
@@ -15,8 +17,8 @@ describe("when input a number then get array type numberString", function() {
 describe("when get a numberArray then get LCDArray", function() {
 
   it("", function() {
-    var input = ["._.|_|..|", ".....|..|", "._.|.||_|"];
-    var result = ["._. ... ._.\n", "|_| ..| |.|\n", "..| ..| |_|\n"]
+    let input = ["._.|_|..|", ".....|..|", "._.|.||_|"];
+    let result = ["._. ... ._.\n", "|_| ..| |.|\n", "..| ..| |_|\n"]
 
     expect(result).toEqual(getLCDArray.getLCDArray(input));
   });
@@ -27,10 +29,10 @@ describe("when get a LCDArray then get LCDString", function() {
 
     spyOn(console, 'log');
 
-    var input = ["._. ... ._.\n", "|_| ..| |.|\n", "..| ..| |_|\n"];
+    let input = ["._. ... ._.\n", "|_| ..| |.|\n", "..| ..| |_|\n"];
     getLCDString.getLCDString(input);
 
-    var result = "._. ... ._.\n|_| ..| |.|\n..| ..| |_|\n"
+    let result = "._. ... ._.\n|_| ..| |.|\n..| ..| |_|\n"
 
     expect(console.log).toHaveBeenCalledWith(result);
   });
